a113b86e05018e5fc187597a9a5b8e72bf40edec
kencormican <ken.cormican@bt.com>
2020-08-08 16:44:31 +0000

    Sample Bar Charts added to googleMaps.js

--- assets/scripts/googleMaps.js
+++ assets/scripts/googleMaps.js
@@ -37,6 +37,8 @@ function setMapData(fromMapArrayParse) {
   });
 
   google.charts.setOnLoadCallback(drawRegionsMap);
+  google.charts.setOnLoadCallback(drawTestBarChartOne);
+  google.charts.setOnLoadCallback(drawTestBarChartTwo);
   google.charts.setOnLoadCallback(drawTestPieChartOne);
   google.charts.setOnLoadCallback(drawTestPieChartTwo);
 
@@ -68,6 +70,95 @@ function setMapData(fromMapArrayParse) {
 
     chart.draw(data, options);
   }
+
+
+
+
+    // Declare function to draw Test Bar Chart One 
+
+    function drawTestBarChartOne() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('timeofday', 'Time of Day');
+      data.addColumn('number', 'Motivation Level');
+
+      data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1],
+        [{v: [9, 0, 0], f: '9 am'}, 2],
+        [{v: [10, 0, 0], f:'10 am'}, 3],
+        [{v: [11, 0, 0], f: '11 am'}, 4],
+        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [13, 0, 0], f: '1 pm'}, 6],
+        [{v: [14, 0, 0], f: '2 pm'}, 7],
+        [{v: [15, 0, 0], f: '3 pm'}, 8],
+        [{v: [16, 0, 0], f: '4 pm'}, 9],
+        [{v: [17, 0, 0], f: '5 pm'}, 10],
+      ]);
+
+      var options = {
+        title: 'Total Country Wide Cases over Time',
+        hAxis: {
+          title: 'Time of Day',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Rating (scale of 1-10)'
+        }
+      };
+
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('ireland-total-cases-graphed'));
+
+      chart.draw(data, options);
+    }
+
+
+        // Declare function to draw Test Bar Chart Two 
+
+    function drawTestBarChartTwo() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('timeofday', 'Time of Day');
+      data.addColumn('number', 'Motivation Level');
+
+      data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1],
+        [{v: [9, 0, 0], f: '9 am'}, 2],
+        [{v: [10, 0, 0], f:'10 am'}, 3],
+        [{v: [11, 0, 0], f: '11 am'}, 4],
+        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [13, 0, 0], f: '1 pm'}, 6],
+        [{v: [14, 0, 0], f: '2 pm'}, 7],
+        [{v: [15, 0, 0], f: '3 pm'}, 8],
+        [{v: [16, 0, 0], f: '4 pm'}, 9],
+        [{v: [17, 0, 0], f: '5 pm'}, 10],
+      ]);
+
+      var options = {
+        title: 'Total Country Wide Deaths over Time',
+        hAxis: {
+          title: 'Time of Day',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Rating (scale of 1-10)'
+        }
+      };
+
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('ireland-total-deaths-graphed'));
+
+      chart.draw(data, options);
+    }
+
 
   
     // Declare function to draw Test Pie Chart One 
