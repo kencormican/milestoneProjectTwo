d9c2809c87a91070d0f7f978bbfc30651c8e4fcc
kencormican <ken.cormican@bt.com>
2020-08-10 14:35:02 +0000

    parseTotalsCasesData() function deserialised and parseIrlBarChartData() function added

--- assets/archive/scriptsArchive/21-getCountryDataSimplify.js
+++ assets/archive/scriptsArchive/21-getCountryDataSimplify.js
@@ -32,47 +32,34 @@ function fetchGeoHiveDataSet() {
 }
 
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
-// Parse Ireland Object Data into Subarrays for further Processing
-
-function parseIrelandData(irelandDataObjFromFetch) {
-    console.dir(irelandDataObjFromFetch);
-    // console dir shows tree to include features and fields objects for data content & headers respectively
-
-    featuresData = irelandDataObjFromFetch.features;                            // features Object array.
-
-    console.log("This is the featuresData Object inside the parseIrelandData() function: ", featuresData);
-
-    var totalsCasesArray = parseTotalsCasesData(featuresData);
-
-    parseIrlHeadlineData(totalsCasesArray);                   // Call parseIrlHeadlineData passing in irelandDataToArray  
-    parseIrlBarChartData(totalsCasesArray);                   // Call parseIrlBarChartData passing in irelandDataToArray             
-}
-
-
-/*----------------------------------------------------------------------------------------------------------------------------------------*/
 // Dynamic Header processing function to return header details to respective calling functions
 
 function parseTableHeaders(obj) {
     var tableHeaders = [];
     //iterate over each obj, retrieve the key and insert it into a table cell
     Object.keys(obj).forEach(function (key) {
-        tableHeaders.push(key)                              // Push keys into tableHeaders array
+        tableHeaders.push(key)
+
     });
     //console.log("This is tableHeaders inside parseTableHeaders: ", tableHeaders)
     return tableHeaders; 
 }
 
 /*----------------------------------------------------------------------------------------------------------------------------------------*/
-// Parse fetaures Object into array for Graphs & Headline Data
+// Parse Ireland Object Data into Subarrays for further Processing
 
-function parseTotalsCasesData(fromParseIrelandData) {
+function parseIrelandData(irelandDataObjFromFetch) {
+    // console.dir(irelandData);
+    // console dir shows tree to include features and fields objects for data content & headers respectively
 
+    var featuresData = irelandDataObjFromFetch.features;                            // features Object array.
     var tableHeaders = [];                                                          // Array of Table Headers
     var tableRows = [];                                                             // Array of Table Rows
-    var totalsCasesToArray = [];                                                    // Concatenated 2D array with header & row data
-
-    // Map fromParseIrelandData object to new object filtering Date, Cases & Deaths key:value pairs 
-    var totalsCasesObject = fromParseIrelandData.map(function (retrieveObjKeyValuePair) {
+    var irelandDataToArray = [];                                                    // Concatenated 2D array with header & row data
+    
+    // Test Manipulation of Array of Objects
+    // Intend on using to extract key:value pairs for production solution
+    var totalsCasesObject = featuresData.map(function (retrieveObjKeyValuePair) {
         return {
             "Date": retrieveObjKeyValuePair.attributes.Date,
             "TotalConfirmedCovidCases": retrieveObjKeyValuePair.attributes.TotalConfirmedCovidCases,
@@ -80,28 +67,28 @@ function parseTotalsCasesData(fromParseIrelandData) {
         }
     });   
 
-    // Extract totalsCasesObject properties to 2D array
+    // Test extract Object properties to 2D array
 
     totalsCasesObject.forEach(function (item) {
         tableRows.push([item.Date, item.TotalConfirmedCovidCases, item.TotalCovidDeaths])
     });
 
-    tableHeaders = parseTableHeaders(totalsCasesObject[0]);     // Pass 1st Index of totalsCasesObject to parseTableHeaders Function
+    tableHeaders = parseTableHeaders(totalsCasesObject[0]);     // Pass 1st Index of totalsCasesObject to parse Header Function
 
     var totalsCasesToArray = tableRows;                         // Initialise totalsCasesToArray with contents of tableRows array
     totalsCasesToArray.unshift(tableHeaders);                   // Insert tableHeaders array into index 0 of totalsCasesToArray
-
     //$.makeArray(irelandDataToArray);
 
     //console.log("This is the parsed totalsCasesObject: ", totalsCasesObject);
     //console.log("This is tableRows inside parseIrelandData(): ", tableRows);
     //console.log("This is tableHeaders inside parseIrelandData(): ", tableHeaders);
-    //console.log("This is totalsCasesToArray inside parseTotalsCasesData(): ", totalsCasesToArray);
+    console.log("This is irelandDataToArray inside parseIrelandData(): ", totalsCasesToArray);
 
-    return totalsCasesToArray;
-    
-
+    parseIrlHeadlineData(totalsCasesToArray);                   // Call parseIrlHeadlineData passing in irelandDataToArray               
 }
+
+
+
 
 
 
@@ -127,23 +114,3 @@ function parseIrlHeadlineData(fromParseIrelandData) {
 }
 
 
-/*----------------------------------------------------------------------------------------------------------------------------------------*/
-// Bar Chart Data Collector
-
-function parseIrlBarChartData(fromParseIrelandData) {
-
-    console.log("parseIrlBarChartData function initiated")
-
-    //console.log("This is the fromParseIrelandData Array inside the parseIrlHeadlineData() function: ",fromParseIrelandData)
-
-    var irlCasesBarChartData = fromParseIrelandData.map(function (item) {
-        return item.slice(0, 2);                            // only retain upto item index 2 in subarray...(so indexes 0 & 1)
-    });
-
-    var irlDeathsBarChartData = fromParseIrelandData.map(function (item) {
-        return (item.slice(0, 1)).concat(item.slice(2,3));                            // concatenates index 0 and 2 of each subarray)
-    });
-
-    console.log("This is the irlCasesBarChartData Array inside the parseIrlBarChartData() function: ", irlCasesBarChartData);
-    console.log("This is the irlDeathsBarChartData Array inside the parseIrlBarChartData() function: ", irlDeathsBarChartData);
-}
